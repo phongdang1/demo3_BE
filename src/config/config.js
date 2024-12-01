b412d10611146c065048e04a3ee939c292d39358
phongdang1 <phongdbde160574@fpt.edu.vn>
2024-12-01 11:39:15 +0700

    2

---
+++ src/config/config.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+    query: {
+      raw: true
+    },
+    timezone: process.env.DB_TIMEZONE
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+    query: {
+      raw: true
+    },
+    timezone: process.env.DB_TIMEZONE
+  }
+};
--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,12 +2,13 @@ const { Sequelize } = require("sequelize");
 
 // Option 2: Passing parameters separately (other dialects)
 const sequelize = new Sequelize(
-  "jobfinder2",
-  "springstudent",
-  "springstudent",
+  process.env.DB_NAME,         // Tên cơ sở dữ liệu từ biến môi trường
+  process.env.DB_USERNAME,     // Tên người dùng cơ sở dữ liệu từ biến môi trường
+  process.env.DB_PASSWORD,     // Mật khẩu cơ sở dữ liệu từ biến môi trường
   {
-    host: "localhost",
-    dialect: "mysql",
+    host: process.env.DB_HOST,     // Host từ biến môi trường
+    port: process.env.DB_PORT,     // Cổng từ biến môi trường
+    dialect: "mysql",              // Loại cơ sở dữ liệu
     logging: false,
   }
 );
